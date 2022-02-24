d6f7f6d5d31dba5d2108a10448b8982213b09bc0
pizzii <dono4executive@gmail.com>
2022-02-25 00:38:56 +0100

    rearrange the files

--- js/admin.js
+++ js/admin.js
@@ -1,11 +1,9 @@
-// Example starter JavaScript for disabling form submissions if there are invalid fields
+// JavaScript for disabling form submissions if there are invalid fields
 ;(function () {
   'use strict'
 
   // Fetch all the forms we want to apply custom Bootstrap validation styles to
   var forms = document.querySelectorAll('.needs-validation')
-  var pass = document.querySelectorAll('password').value
-  var cpass = document.querySelectorAll('cnpassword').value
 
   // Loop over them and prevent submission
   Array.prototype.slice.call(forms).forEach(function (form) {
@@ -14,10 +12,14 @@
       function (event) {
         if (!form.checkValidity()) {
           event.preventDefault()
+          // let pass = document.getElementById('password').value
+          // let cpass = document.getElementById('cnpassword').value
+          // if (pass === cpass) {
+          //   console.log('pass')
+          // } else {
+          //   console.log('check password')
+          // }
           event.stopPropagation()
-          if (pass != cpass) {
-            alert('check password')
-          }
         }
 
         form.classList.add('was-validated')
@@ -27,61 +29,23 @@
   })
 })()
 
-// $(function () {
-//   $('#submit').on('click', function (event) {
-//     event.preventDefault()
-//   })
-// })
-// $(function () {
-//   $("form[name='registration']").validate({
-//     rules: {
-//       firstname: "required",
-//       lastname: "required",
-//       department: "required",
-//       matno: "required",
-//       phone: "required",
-//       email: {
-//         required: true,
-//         email: true
-//       },
-//       password: {
-//         require: true,
-//         minlenght: 5
-//       },
-//       cnpassword: {
-//         require: true,
-//         minlenght: 5,
-//         equalTo: "#password"
-//       }
-//     },
-//     messages: {
-//       firstname: "Please enter your first name",
-//       lastname: "Please enter your last name",
-//       department: "Please enter your department",
-//       matno: "Please enter your first name",
-//       phone: "Please enter your first name",
-//       password: {
-//         required: "please provide a password",
-//         minlenght: "your must be at least 5 character"
-//       },
-//       cnpassword: {
-//         required: "please provide a password",
-//         minlenght: "your must be at least 5 character",
-//         equalTo: "please enter the same password as above"
-//       },
-//       email: "please enter a valid email"
-//     },
-//     submitHandler: function (form) {
-//       form.submit()
-//     }
-//   })
-// })
-
+//home page
 $(function () {
   $('#log').on('click', function () {
     location = 'userlogin.html'
   })
   $('#sign').on('click', function () {
     location = '#account'
+  })
+  $('.btn').on('click', function (e) {
+    // e.preventDefault()
+    let pass = $('#password').val()
+    let cpass = $('#cnpassword').val()
+
+    if (pass === cpass) {
+      console.log('pass')
+    } else {
+      console.log('check password')
+    }
   })
 })
